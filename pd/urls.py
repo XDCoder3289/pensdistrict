93860eaf9910b5a2e19ecddbf4f4b65d676b5d95
XDCoder3289 <xdcoder3289@gmail.com>
2020-11-08 17:04:28 +0500

    a few changes made

--- pd/urls.py
+++ pd/urls.py
@@ -9,6 +9,9 @@ app_name = 'pd'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('privacy-policy/', views.privacy, name='privacy'),
+    path('contact/', views.contact, name='contact'),
     path('<slug:cat_slug_name>/', views.cat_page, name='cat_page'),
     path('<slug:cat_slug_name>/<slug:post_slug_name>/', views.posts, name='posts'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
