67b76d9e257b4f2e79bda11139c80a0e1c2a6e40
XDCoder3289 <xdcoder3289@gmail.com>
2020-11-09 16:20:05 +0500

    changing the local setup

--- pensdistrict/settings.py
+++ pensdistrict/settings.py
@@ -29,16 +29,16 @@ with open('secret.key') as f:
 SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 CSRF_COOKIE_SECURE = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
-SECURE_SSL_REDIRECT = True
+# SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 X_FRAME_OPTIONS = 'DENY'
 
 
-ALLOWED_HOSTS = ['pensdistrict.com']
+ALLOWED_HOSTS = ['pensdistrict.com', '127.0.0.1']
 
 
 # Application definition
