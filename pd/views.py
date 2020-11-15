bb571f4353665a071efd3d6b204b734af8707509
XDCoder3289 <xdcoder3289@gmail.com>
2020-11-15 19:07:15 +0500

    Adding the Top Picks Category

--- pd/views.py
+++ pd/views.py
@@ -6,11 +6,13 @@ from pd.models import PenInPost
 
 def index(request):
     context_dict = {}
+    cat_for_nav = Category.objects.all()
     # Latest Posts
     categories = Category.objects.all()[:3]
     posts = Post.objects.all()[:3]
-    # Posts for Best
-    posts_for_best = Post.objects.all()[3:6]
+    # Posts for Top Picks
+    cat_for_top = Category.objects.get(pen_category='Top Picks')
+    posts_for_best = Post.objects.filter(category=cat_for_top)[:3]
     # Premium Pens
     cat_for_premium = Category.objects.get(pen_category='Premium Pens')
     posts_for_premium = Post.objects.filter(category=cat_for_premium)[:3]
@@ -22,6 +24,7 @@ def index(request):
     posts_for_quills = Post.objects.filter(category=quill_pens)[:3]
 
     # context_dict
+    context_dict['category_nav'] = cat_for_nav
     context_dict['categories'] = categories
     context_dict['posts'] = posts
     context_dict['best'] = posts_for_best
@@ -41,7 +44,7 @@ def cat_page(request, cat_slug_name):
         posts = Post.objects.filter(category=category)
         context_dict['category'] = category
         context_dict['posts'] = posts
-        context_dict['categories'] = categories
+        context_dict['category_nav'] = categories
     except:
         context_dict['not_found'] = "Sorry, the page does not exist"
     return render(request, 'categories.html', context=context_dict)
@@ -54,7 +57,7 @@ def posts(request, cat_slug_name, post_slug_name):
         pens_in_post = PenInPost.objects.filter(pen_is_in=direct_post)
         category = Category.objects.all()
         side_nav_posts = Post.objects.exclude(post_name=direct_post)[:5]
-        context_dict['categories'] = category
+        context_dict['categor_nav'] = category
         context_dict['post'] = direct_post
         context_dict['pens'] = pens_in_post
         context_dict['sidenav_posts'] = side_nav_posts
@@ -66,22 +69,22 @@ def posts(request, cat_slug_name, post_slug_name):
 def about(request):
     context_dict = {}
     categories = Category.objects.all()
-    context_dict['categories'] = categories
+    context_dict['category_nav'] = categories
     return render(request, 'about.html', context=context_dict)
 
 def privacy(request):
     context_dict = {}
     categories = Category.objects.all()
-    context_dict['categories'] = categories
+    context_dict['category_nav'] = categories
     return render(request, 'privacy.html', context=context_dict)
 
 
 def contact(request):
     context_dict = {}
     categories = Category.objects.all()
-    context_dict['categories'] = categories
+    context_dict['category_nav'] = categories
     return render(request, 'contact.html', context=context_dict)
 
 def sitemap(request):
     context_dict = {}
-    return render(request, 'sitemap.xml', context=context_dict)    
+    return render(request, 'sitemap.xml', context=context_dict)
