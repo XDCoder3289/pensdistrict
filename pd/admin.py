753d55e0166306fb958e2872ade6fc144fcd631f
XDCoder3289 <xdcoder3289@gmail.com>
2020-11-15 21:12:32 +0500

    Adding the search functionality in admin

--- pd/admin.py
+++ pd/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from pd.models import Category, Post, PenInPost
 
+class PenInPostAdmin(admin.ModelAdmin):
+    search_fields = ('pen_name',)
+
 admin.site.register(Category)
 admin.site.register(Post)
-admin.site.register(PenInPost)
+admin.site.register(PenInPost, PenInPostAdmin)
